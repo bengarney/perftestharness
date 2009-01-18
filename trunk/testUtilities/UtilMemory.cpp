c00f3dcf45690318850391411a4affa3db9a5748
eric.preisz <eric.preisz@696888a4-9348-11dd-afb0-39fe2c1da4cb>
2009-01-18 19:30:33 +0000

    added some unrolling tests

--- trunk/testUtilities/UtilMemory.cpp
+++ trunk/testUtilities/UtilMemory.cpp
@@ -3,8 +3,11 @@
 
 
 
-#define DATA_SIZE (1024 * 1024)
-__declspec(align(16)) static float gStaticData1024x1024[DATA_SIZE];
+
+__declspec(align(16)) static float gStaticData1024x1024[(1024 * 1024)];
+
+static int gIStaticData64Bytes[16];
+static float gFStaticData64Bytes[16];
 
 UtilMemory::UtilMemory()
 {
@@ -19,4 +22,14 @@ UtilMemory::~UtilMemory()
 float* UtilMemory::GetStatic1024x1024_16_Aligned()
 {
 	return gStaticData1024x1024;
+}
+
+int* UtilMemory::GetIStatic64Bytes()
+{
+	return gIStaticData64Bytes;
+}
+
+float* UtilMemory::GetFStatic64Bytes()
+{
+	return gFStaticData64Bytes;
 }
